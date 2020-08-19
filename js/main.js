31cc22c7ba80eb7d946489bcafc42df774f25b58
Mai Yamawaki <joooooy031006@icloud.com>
2020-08-19 10:47:47 -0500

    html css editado

--- js/main.js
+++ js/main.js
@@ -10,4 +10,30 @@ const naruto = new Arma()
 
 
     
+//btn start principal  page
+const btnCredit=document.querySelector(".creditbtn")
+const btnBack=document.querySelector(".back");
+const btnStart=document.querySelector(".start");
+const canvasClass=document.querySelector(".canvas")
 
+const main=document.querySelector("main")
+const credit=document.querySelector(".credit")
+
+btnCredit.addEventListener("click",()=>{
+  main.classList.add("invisible")
+  credit.classList.remove("invisible")
+  credit.classList.add("visible")
+})
+
+btnBack.addEventListener("click",()=>{
+  main.classList.remove("invisible")
+  main.classList.add("visible")
+  credit.classList.remove("visible")
+  credit.classList.add("invisible")
+})
+
+btnStart.addEventListener("click",()=>{
+  main.classList.add("invisible")
+  canvasClass.classList.remove("invisible")
+  canvasClass.classList.add("visible")
+})
